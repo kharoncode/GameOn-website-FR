0db6dd8bb68f59994e00123205472c62c8777e7a
Remi Collin <sharon.code@proton.me>
2023-03-31 15:31:26 +0200

    clean code 80% Need to fix submitValidity function

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -26,31 +26,36 @@
     const modalCOS = document.querySelector('input[id="checkbox1"]');
     const modalNewsletter = document.querySelector('input[id="checkbox2"]');
 
-// OPEN/CLOSE MODAL
-  // launch modal event
-  modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-  // launch modal form
-  function launchModal() {
-    modalbg.style.display = "block";
-    if(modalForm.reportValidity() === true) {
-      modalSubmit.style.background = "#fe142f";
-    }else{
-      modalSubmit.style.background = "#7c4349";
-    }
+// btn-submit color change if formvalidity=true/false
+function submitColorValidity(){
+  if(modalForm.reportValidity()) {
+    modalSubmit.style.background = "#fe142f";
+  }else{
+    modalSubmit.style.background = "#7c4349";
   }
+} 
 
-  // close modal event
+// OPEN/CLOSE MODAL
+  // --launch modal event
+    modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+    // --launch modal form
+    function launchModal() {
+      modalbg.style.display = "block";
+      submitColorValidity();
+    }
+
+  // --close modal event
     modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
     document.querySelector(".confirmation-btn").addEventListener("click", closeModal);
 
-  // close modal form
+  // --close modal form
     var modalContent = document.querySelector(".content");
-    // --Get var(--modal-duration)
+    // --Get var(--modal-duration) and convert it for JS
       var modalContentDuration = getComputedStyle(modalContent).getPropertyValue("animation-duration").match(/[a-zA-Z]/);
       if(modalContentDuration == "s") {
         modalContentDuration = getComputedStyle(modalContent).getPropertyValue("animation-duration").match(/\d+.\d+|\d+/)*1000;
-      } else if (modalContentDuraton == "ms"){
+      } else if (modalContentDuration == "ms"){
         modalContentDuration = getComputedStyle(modalContent).getPropertyValue("animation-duration").match(/\d+.\d+|\d+/);
       }
     // --Function closeModal
@@ -58,10 +63,9 @@
         modalContent.style.animation = "modalclose var(--modal-duration) both";
         setTimeout(() => { 
           modalbg.style.display ="none";
-          modalContent.style.animation = "modalopen var(--modal-duration)";
+          modalContent.style.animation = "modalopen var(--modal-duration)"; // reset animation
         }, modalContentDuration);
       }
-
 
 // Link formData to a Name in an Object
   // --Get all input name
@@ -71,50 +75,41 @@
       inputName.push(key.name)
     }
     inputName = [...new Set(inputName)]; // del duplicate
-    /* function inputNameAdd (){
-      formDataInput.forEach(e => {
-        inputName.push(e.name);
-      });
-      inputName = [...new Set(inputName)]; // del duplicate
-    }
-    inputNameAdd(); */
   // --Add an Object "formDataObject" : key=inputName value=formData
     var formDataObject = new Object();
     for (let i = 0; i<inputName.length; i++){
         formDataObject[`${inputName[i]}`]=formData[i];
       };
 
-
 // Error Messages
   const errorMessagesObject = {
     first : "Veuillez entrer 2 caractères ou plus pour le champ du prénom.",
     last : "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
-    email : "Veuillez entrer une adresse mail valide 'Ex: jean@doe.com'.",
+    email : "Veuillez entrer une adresse mail valide. Ex: game@on.com",
     birthdate : "Veuillez entrer une date de naissance valide.",
-    quantity : "Vous devez indiquer un nombre entre 1 et 99",
-    location : "Vous devez choisir une des options.",
-    cos : "Vous devez vérifier que vous acceptez les termes et conditions.",
+    quantity : "Indiquez un nombre entre 1 et 99",
+    location : "Choisissez une des options.",
+    cos : "Vérifiez que vous acceptez les conditions d'utilisation.",
   }
+  // --add to each FormData his ErrorMessage
   for (const key in errorMessagesObject) {
       const element = errorMessagesObject[key];
       formDataObject[key].dataset.error = element;
   }
 
-
 // Custom Validity 
   // --Mail
     modalMail.setAttribute('pattern', "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}");
   // --Birthdate
-    const date = new Date();
-    let day = date.getDate();
+    const date = new Date(); // Get current date
+    let day = date.getDate(); // Get Day
     if(day<10){day=`0${day}`;}
-    let month = date.getMonth()+1;
+    let month = date.getMonth()+1; // Get Month
     if(month<10){month=`0${month}`;}
-    let year = date.getFullYear();
-    let currentDate = `${year}-${month}-${day}`;
+    let year = date.getFullYear(); // Get Year
+    let currentDate = `${year}-${month}-${day}`; // Concatenation
     modalBirthdate.setAttribute('max', currentDate);
     modalBirthdate.setAttribute('min', "1907-03-04"); // Birthdade of the oldest woman on earth
-
 
 // Show ErrorMessage for each formData
   function formDataValidity (key, modal){
@@ -125,56 +120,52 @@
     }
   }
   // --First Name
-  modalFirstName.addEventListener("keyup", (e)=>{
-    formDataValidity("first", e);
-  }); 
+    modalFirstName.addEventListener("keyup", (e)=>{
+      formDataValidity("first", e);
+    }); 
   // --Last Name
-  modalLastName.addEventListener("keyup", (e)=>{
-    formDataValidity("last", e);
-  });
+    modalLastName.addEventListener("keyup", (e)=>{
+      formDataValidity("last", e);
+    });
   // --Email
-  modalMail.addEventListener("keyup", (e)=>{
-    formDataValidity("email", e);
-  });
+    modalMail.addEventListener("keyup", (e)=>{
+      formDataValidity("email", e);
+    });
   // --Birthdate
-  modalBirthdate.addEventListener("change", (e)=>{
-    formDataValidity("birthdate", e);
-  });
+    modalBirthdate.addEventListener("change", (e)=>{
+      formDataValidity("birthdate", e);
+    });
   // --Quantity
-  modalQuantity.addEventListener("keyup", (e)=>{
-    formDataValidity("quantity", e);
-  });
+    modalQuantity.addEventListener("keyup", (e)=>{
+      formDataValidity("quantity", e);
+    });
   // --Location
-  const modalInputLocation = document.getElementById('location1');
-  modalSubmit.addEventListener("mouseover", ()=>{
-    if(modalInputLocation.validity.valid){
-      delete formDataObject['location'].dataset.errorVisible;
-    }else{
-      formDataObject['location'].dataset.errorVisible = true;
-    };
-  });
+    const modalInputLocation = document.getElementById('location1');
+    modalSubmit.addEventListener("mouseover", ()=>{
+      if(modalInputLocation.validity.valid){
+        delete formDataObject['location'].dataset.errorVisible;
+      }else{
+        formDataObject['location'].dataset.errorVisible = true;
+      };
+    });
 
   // --Conditions of Sale
-  var cosValidity = modalCOS.validity.valid;
-  modalCOS.addEventListener("change", (e)=>{
-    formDataValidity("cos", e);
-  });
+    var cosValidity = modalCOS.validity.valid;
+    modalCOS.addEventListener("change", (e)=>{
+      formDataValidity("cos", e);
+    });
 
 // Submit modal
-  // -- Validity Test
+  // -- Validity Test : Submit color change if formvalidity=true/false
     modalSubmit.addEventListener("mouseover", ()=>{
-      if(modalForm.reportValidity()) {
-        modalSubmit.style.background = "#fe142f";
-      }else{
-        modalSubmit.style.background = "#7c4349";
-      }
+      submitColorValidity();
     });
   // -- Onsubmit
     function validate(event){
-      event.preventDefault()
+      event.preventDefault() // stop default event of onsubmit
       modalForm.style.animation = "formhidden var(--modal-duration) both";
       setTimeout(() => { 
-        modalForm.classList.add('select-hide');
+        modalForm.classList.add('select-hide'); // display=none;
         document.querySelector(".confirmation-container").style.display = "flex";
         document.querySelector(".confirmation-container").style.animation = "confirmationanim var(--modal-duration) both";
         }, modalContentDuration);
